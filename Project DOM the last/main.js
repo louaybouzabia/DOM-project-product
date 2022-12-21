fda7ba6b741c839221f0aa070c28f3d9dec58b7b
louaybouzabia <115157167+louaybouzabia@users.noreply.github.com>
2022-12-21 10:24:38 +0100

    Add files via upload

---
+++ Project DOM the last/main.js
@@ -0,0 +1,131 @@
+let title = document.getElementById('title');
+let price = document.getElementById('price');
+let taxes = document.getElementById('taxes');
+let ads = document.getElementById('ads');
+let discount = document.getElementById('discount');
+let total = document.getElementById('total');
+let count = document.getElementById('count');
+let category = document.getElementById('category');
+
+// get total 
+
+function getTotal(){
+    if(price.value != '')
+    {
+        let result = (+price.value + +taxes.value + +ads.value) - +discount.value ;
+        total.innerHTML = result ;
+        total.style.background = '#040';
+    }else{
+        total.innerHTML = '';
+        total.style.background = '#d23025ce';
+    }
+}
+
+
+
+// create product AND save localstorage
+
+let dataPro;
+if(localStorage.product != null)
+{
+    dataPro = JSON.parse(localStorage.product);
+}
+else
+{
+    dataPro = [];
+}
+
+submit.onclick = function()
+{
+    let newPro = 
+    {
+        title:title.value,
+        price:price.value,
+        taxes:taxes.value,
+        ads:ads.value,
+        discount:discount.value,
+        total:total.innerHTML,
+        count:count.value,
+        category:category.value,
+    }    
+
+    if(newPro.count > 1){
+        for(let i = 0; i < newPro.count ; i++){
+            dataPro.push(newPro);
+
+        }
+    }else{
+        dataPro.push(newPro);
+    }
+        
+    dataPro.push(newPro);
+    localStorage.setItem('product', JSON.stringify(dataPro));
+    clearData();
+    showData();
+}
+// clear inputs 
+
+function clearData()
+{
+    title.value = '';
+    price.value = '';
+    taxes.value = '';
+    ads.value = '';
+    discount.value = '';
+    total.innerHTML = '';
+    count.value = '';
+    category.value = '';
+}
+
+// show the Data
+
+function showData()
+{
+    let table = '';
+    for(let i = 0; i < dataPro.length; i++)
+    {
+        table += `
+        <tr>
+           <td>${i+1}</td>
+           <td>${dataPro[i].title}</td>
+           <td>${dataPro[i].price}</td>
+           <td>${dataPro[i].taxes}</td>
+           <td>${dataPro[i].ads}</td>
+           <td>${dataPro[i].discount}</td>
+           <td>${dataPro[i].total}</td>
+           <td>${dataPro[i].category}</td>
+           <td><button id="update">update</button></td>
+           <td><button onclick="deleteData(${i})" id="delete">delete</button></td>
+        </tr>
+        `
+    }
+    document.getElementById('tbody').innerHTML = table;
+    let btnDelete = document.getElementById('deleteAll');
+    if(dataPro.length > 0)
+    {
+        btnDelete.innerHTML =   `    <button onclick="deleteAll()" >delete All(${dataPro.length})</button>     `;
+
+    }
+    else
+    {
+        btnDelete.innerHTML = '';
+    }
+}
+showData();
+
+
+// delete 
+function deleteData(i)
+{
+    dataPro.splice(i,1);
+    localStorage.product = JSON.stringify(dataPro);
+    showData()
+}
+
+function deleteAll()
+    {
+        localStorage.clear()
+        dataPro.splice(0)
+        showData()
+    }
+deleteAll()
